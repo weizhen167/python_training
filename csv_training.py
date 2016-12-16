f9d3741b3994825568d2ab240e55489b97a2bd5a
weizhen167 <weizhen167@gmail.com>
2016-12-16 15:10:54 -0500

    212

---
+++ csv_training.py
@@ -0,0 +1,19 @@
+
+import csv
+
+# Exercise 1 : Simple Reader
+
+
+# Exercise 2a : Format keyword arguments
+
+
+# Exercise 2b : Dialect
+
+
+# Exercise 3a : Index Reader
+
+# Exercise 3b : Index Writer
+
+# Exercise 5 : DictReader
+
+# Exercise 5b : DictWriter
