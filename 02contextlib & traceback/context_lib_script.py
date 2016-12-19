3eeaf4ae63023b16a4db2950b2df7332e42f89da
weizhen167 <weizhen167@gmail.com>
2016-12-19 15:38:44 -0500

    0.6

--- 02contextlib & traceback/context_lib_script.py
+++ 02contextlib & traceback/context_lib_script.py
@@ -1,13 +1,36 @@
 import contextlib
-import traceback
-import sys
+
 
 # Create context without use of contextlib
+class context_class(object):
+    def  __init__(self):
+        print 'init'
+    def __enter__(self):
+        print 'enter'
+        return self
+    def __exit__(self,exc_type, exc_val,exc_tb):
+        print 'exit'
 
+with context_class() as a:
+    print 'in the context'
 # Create context with context lib
+@contextlib.contextmanager
+def xml_tag(tag_name):
+    print '<%s>' %tag_name
+    yield tag_name
+    print '</%s>' %tag_name
+
+with xml_tag('tag_1'):
+    print 'in the context'
 
 # Nested contexts
+@contextlib.contextmanager
+def xml_tag_1(tag_name,fd):
+    fd.write('<%s>\n' %tag_name)
+    yield tag_name
+    fd.write('\n</%s>' % tag_name)
 
-# Using Traceback (traceback.print_exc, format_exc)
+with open("output.txt", "w") as fd:
+    with xml_tag_1('tag_1',fd) :
+        fd.write('hello_world')
 
-# Stack traces
---
+++ 02contextlib & traceback/traceback.py
@@ -0,0 +1,10 @@
+import traceback
+import sys
+
+
+
+# Using Traceback (traceback.print_exc, format_exc)
+
+
+
+# Stack traces
