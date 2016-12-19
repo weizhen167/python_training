8d45a3efa4251d9854624ba436fd9fe34b529635
weizhen167 <weizhen167@gmail.com>
2016-12-19 15:10:42 -0500

    0.5

---
+++ 02contextlib & traceback/context_lib_script.py
@@ -0,0 +1,13 @@
+import contextlib
+import traceback
+import sys
+
+# Create context without use of contextlib
+
+# Create context with context lib
+
+# Nested contexts
+
+# Using Traceback (traceback.print_exc, format_exc)
+
+# Stack traces
