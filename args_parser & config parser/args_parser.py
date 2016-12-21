cd20574bcaeea71499ada061f034befbf76aa343
weizhen167 <weizhen167@gmail.com>
2016-12-21 17:26:01 -0500

    1

--- args_parser & config parser/args_parser.py
+++ args_parser & config parser/args_parser.py
@@ -15,6 +15,9 @@ print "print each args"
 for i, eachArg in enumerate(sys.argv):
     print "[%d] = %s" % (i, eachArg)
 '''
+
+
+
 '''
 # set dest = "echo"
 # our parser could read parameter "echo" from the input parameter
@@ -29,16 +32,36 @@ print "parameter types", type(args)
 print "show string echo = ", args.echo   # use args.echo get parameter info
 '''
 
-parser.add_argument(dest="m", help="enter the m...")
-parser.add_argument(dest="n", help="enter the n...")
+#set a
+'''
+parser.add_argument(dest="m", help="enter the m...",type = int)
+parser.add_argument(dest="n", help="enter the n...",type = int)
 args = parser.parse_args()
-m_ = int(args.m)
-n_ = int(args.n)
-print "%d ^ %d = %d" % (m_, n_, m_ ** n_)
+m = args.m
+n = args.n
+print "%d ^ %d = %d" % (m, n, m ** n)
 
+
+parser.add_argument("-m", "--m_number", dest = "m", help = "enter the m...", type = int)
+parser.add_argument("-n", "--n_number", dest = "n", help = "enter the n...", type = int)
+parser.add_argument("-w", "--w_number", dest = "w", help = "enter the w...")
+args = parser.parse_args()
+#print args.w
+print "%d ^ %d = %d" % (args.m,  args.n, args.m ** args.n + args.w)
 '''
 
+parser = argparse.ArgumentParser(description='Example with long option names')
 
+parser.add_argument('--noarg', action="store_true", default=False)
+parser.add_argument('--witharg', action="store", dest="witharg")
+parser.add_argument('--witharg2', action="store", dest="witharg2", type=int)
+
+print parser.parse_args(['--noarg', '--witharg', 'val', '--witharg2=3'])
+
+
+
+
+'''
 if __name__ == "__main__":
     #part1 intro
     print "message has type %s, type %s"  % (sys.argv, len(sys.argv))
@@ -46,5 +69,4 @@ if __name__ == "__main__":
     print "print info of each args"
     for i, eachArg in enumerate(sys.argv):
         print "[%d] = %s" % (i, eachArg)
-
 '''
