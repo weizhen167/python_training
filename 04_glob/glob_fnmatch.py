dcc3575882dece72a0bcdf19ded2afa858dba1cf
weizhen167 <weizhen167@gmail.com>
2016-12-22 15:07:27 -0500

    new changes

---
+++ 04_glob/glob_fnmatch.py
@@ -0,0 +1,25 @@
+import fnmatch
+import glob
+import os
+import shutil
+
+import re
+
+# Excersise 1 Capture all the csv files using glob 
+
+
+# Excersise 2 Capture all of the "e" text files  glob iterator 
+
+
+# Excersise 3 Capture all csv files 2-5 using fnmatch 
+
+
+# Excersise 4 Capture all the Text files 1-2 c and e fnmatch filter 
+
+
+# Excersise 5 Regex conversion. 
+
+
+
+
+
